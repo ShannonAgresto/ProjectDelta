355d5181795ce0d36cee03eafac6de7e0d373f9a
Shannon Agresto <47060951+ShannonAgresto@users.noreply.github.com>
2019-04-11 19:33:36 -0400

    Add files via upload

---
+++ mac_db2.sql
@@ -0,0 +1,10 @@
+USE mac_db2;
+
+SELECT* from people;
+
+INSERT INTO people (owner_name, has_pet, pet_name, pet_age)
+VALUES ('Shannon', true, 'Mackenzie', 3);
+
+INSERT INTO people (owner_name, has_pet, pet_name, pet_age)
+VALUES ('Katie', true, 'Rambo', 5);
+
